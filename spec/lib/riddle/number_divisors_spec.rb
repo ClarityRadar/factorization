5d80b80ece7c4c788ceb8c665322f8f7d5375e1d
Georgi N. Kostov <gnkostov@yahoo.com>
2017-03-13 17:21:37 +0200

    Add missing specs for Riddle::NumberDivisors

--- spec/lib/riddle/number_divisors_spec.rb
+++ spec/lib/riddle/number_divisors_spec.rb
@@ -1,9 +1,29 @@
 require 'spec_helper'
 
 RSpec.describe Riddle::NumberDivisors do
+  subject { described_class.new(number) }
+
   describe '#to_a' do
+    context 'when number is equal to 170170' do
+      let(:number) { 170_170 }
+
+      it 'is an instance of Array' do
+        expect(subject.to_a).to be_an_instance_of(Array)
+      end
+
+      it 'is nonempty' do
+        expect(subject.to_a).to_not be_empty
+      end
+    end
   end
 
   describe '#size' do
+    context 'when number is equal to 170170' do
+      let(:number) { 170_170 }
+
+      it 'has a size of 64' do
+        expect(subject.size).to eq 64
+      end
+    end
   end
 end
