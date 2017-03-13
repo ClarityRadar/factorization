ceceddcb03e447f3f12a9259cef2cc97b2658786
Georgi N. Kostov <gnkostov@yahoo.com>
2017-03-13 12:04:33 +0200

    Improve README

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Riddle
 
-TODO: Write a better README
+Every number could be decomposed as a multiplication of prime numbers on particular powers.
+
+For a given number finds the list of all prime divisors, and their corresponding powers.
+
+For example the number 720_720, has this list as an output:
+`[[2, 4], [3, 2], [5, 1], [7, 1], [11, 1], [13, 1]]`
+
+The first element of each tuple is a prime divisor of 720_720
+and the second element - the power of that divisor.
+
+```
+720_720 == (2 ** 4) * (3 ** 2) * (5 ** 1) * (7 ** 1) * (11 ** 1) * (13 ** 1)
+```
 
 ```ruby
 tuples = Riddle::Tuples.new(720_720)
--- lib/riddle.rb
+++ lib/riddle.rb
@@ -3,18 +3,10 @@ require './lib/riddle/tuples'
 require './lib/riddle/number_divisors'
 require './lib/riddle/prime_divisors'
 
-# For a given number find the list of prime divisors,
-# and their corresponding powers, since every number could be
-# decomposed as a multiplication of prime numbers on particular powers.
-# For example the number 720_720, has this list as an output:
-# [[2, 4], [3, 2], [5, 1], [7, 1], [11, 1], [13, 1]],
-# where the first element of each tuple is a prime divisor of 720_720
-# and the second element - the power of that divisor.
-# 720_720 == (2 ** 4) * (3 ** 2) * (5 ** 1) * (7 ** 1) * (11 ** 1) * (13 ** 1)
-module Riddle
-end
+# module Riddle
+# end
 
-tuples = Riddle::Tuples.new(720_720)
-tuples_list = tuples.to_a
+# tuples = Riddle::Tuples.new(720_720)
+# tuples_list = tuples.to_a
 
-puts tuples_list.to_s # => [[2, 4], [3, 2], [5, 1], [7, 1], [11, 1], [13, 1]]
+# puts tuples_list.to_s # => [[2, 4], [3, 2], [5, 1], [7, 1], [11, 1], [13, 1]]
