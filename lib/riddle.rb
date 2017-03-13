d7efd23f767e61a5079a1fd253b7c40150e8a8c7
Georgi N. Kostov <gnkostov@yahoo.com>
2017-03-13 12:05:42 +0200

    delete useless code

--- lib/riddle.rb
+++ lib/riddle.rb
@@ -2,11 +2,3 @@ require './lib/riddle/filler'
 require './lib/riddle/tuples'
 require './lib/riddle/number_divisors'
 require './lib/riddle/prime_divisors'
-
-# module Riddle
-# end
-
-# tuples = Riddle::Tuples.new(720_720)
-# tuples_list = tuples.to_a
-
-# puts tuples_list.to_s # => [[2, 4], [3, 2], [5, 1], [7, 1], [11, 1], [13, 1]]
